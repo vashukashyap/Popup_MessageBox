dd06e46881db053a2b10fc04a8cdead830bb15fd
vashu kashyap <kashyapvashu2003@gmail.com>
2023-11-01 01:27:19 +0530

    first-commit

---
+++ sciprt.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById("popup");
+
+const openPopup = () => {
+    popup.classList.add("open-popup")
+}
+
+const closePopup = () => {
+    popup.classList.remove("open-popup")
+}
